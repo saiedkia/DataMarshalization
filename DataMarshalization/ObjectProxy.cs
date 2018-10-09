27b92f55d0f1eb4a7f2f59d7f040c9098327af18
skia <saiedkia@outlook.com>
2018-10-09 23:13:29 +0330

    init commit

---
+++ DataMarshalization/ObjectProxy.cs
@@ -0,0 +1,6 @@
+﻿namespace DataMarshalization
+{
+    internal class ObjectProxy
+    {
+    }
+}
---
+++ DataMarshalization/Writer.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataMarshalization
+{
+    public class Writer
+    {
+    }
+}
