cfc045d6f36be3e1367e9ead7509b4baa1ee659c
skia <saiedkia@outlook.com>
2018-10-11 00:55:27 +0330

    first implementation

--- src/DataMarshalization/Reader.cs
+++ src/DataMarshalization/Reader.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace DataMarshalization
 {
-    class Reader
+    public static class Reader
     {
+        public static T Read<T>(string path)
+        {
+            byte[] bytes = File.ReadAllBytes(path);
+            return Read<T>(bytes);
+        }
+
+        public static T Read<T>(byte[] data)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+
+                BinaryFormatter formatter = new BinaryFormatter();
+                memory.Write(data, 0, data.Length);
+                memory.Seek(0, SeekOrigin.Begin);
+                object x = formatter.Deserialize(memory);
+                return (T)formatter.Deserialize(memory);
+            }
+        }
     }
 }
--- src/DataMarshalization/Writer.cs
+++ src/DataMarshalization/Writer.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace DataMarshalization
 {
-    public class Writer
+    public static class Writer
     {
+        public static byte[] Serialize(object @object)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                Type objectType = @object.GetType();
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(memory, @object);
+
+                return memory.ToArray();
+            }
+        }
+
+        public static bool Save(byte[] data, string path)
+        {
+            return true;
+        }
     }
 }
